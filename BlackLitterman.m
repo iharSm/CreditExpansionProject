d6b5bd82ae746719bbac611754f56fee5b6256d0
iharSm <isdroidmail@gmail.com>
2017-05-11 21:34:21 -0400

    clean up

--- BlackLitterman.m
+++ BlackLitterman.m
@@ -1,49 +1,67 @@
 classdef BlackLitterman
-    %BlackLitterman Summary of this class goes here
-    %   Detailed explanation goes here
     
     properties
-        fileName = 'data/bank_equity_returns_3y.xlsx';
+        predictions_file;
+        historical_returns_file;
         mu_credit_exp;
         historical_returns;
-        dates
+        dates;
+        risk_free_rate;
+        number_of_obs;
+        portfolio_size;
     end
     
     methods
-        function obj = BlackLitterman()
-            obj.dates = sort([datetime([1920:2013]', 1, 1);
-                datetime([1920:2013]', 4, 1);
-                datetime([1920:2013]', 7, 1);
+        function obj = BlackLitterman(predictions_file, countries, historical_returns_file, historical_returns_sheet, frequency)
+            obj.predictions_file = predictions_file;
+            obj.historical_returns_file = historical_returns_file;
+            
+            obj.dates = sort([datetime([1920:2012]', 1, 1);
+                datetime([1920:2012]', 4, 1);
+                datetime([1920:2012]', 7, 1);
                 datetime([1920:2012]', 10, 1)]);
-            [m,n] = size(obj.dates);
-            tr = timerange('01/01/1974','04/01/2013');
             
-            obj.mu_credit_exp = readtable(obj.fileName,'Sheet','mean');
+            tr = timerange('01/01/1976','10/01/2012');
             
+            obj.mu_credit_exp = readtable(obj.predictions_file,'Sheet','mean');
             obj.mu_credit_exp.Var1 = [];
             obj.mu_credit_exp = table2timetable(obj.mu_credit_exp,'RowTimes',obj.dates);
+            obj.mu_credit_exp = obj.mu_credit_exp(:,countries);
             
             obj.mu_credit_exp = obj.mu_credit_exp(tr,:);
+            func = @(x)(1+x).^(1/frequency)-1;
+            temp = obj.mu_credit_exp.Properties.VariableNames;
+            obj.mu_credit_exp = varfun(func,obj.mu_credit_exp(2:end,:));
+            obj.mu_credit_exp.Properties.VariableNames = temp;
             
+            [obj.number_of_obs, obj.portfolio_size] = size(obj.mu_credit_exp);
             
-            obj.historical_returns = readtable('data/histdata.xlsx', 'Sheet','equity_tot_excess_return');
+            obj.historical_returns = readtable(historical_returns_file, 'Sheet', historical_returns_sheet);
             obj.historical_returns.Var1 = [];
             obj.historical_returns = table2timetable(obj.historical_returns,'RowTimes',obj.dates);
-            obj.historical_returns = obj.historical_returns(tr,:);
+            obj.historical_returns = obj.historical_returns(tr, countries);
             
-            obj.dates = sort([datetime([1974:2013]', 1, 1);
-                datetime([1974:2012]', 4, 1);
-                datetime([1974:2012]', 7, 1);
-                datetime([1974:2012]', 10, 1)]);
+            obj.risk_free_rate = readtable('data/historical data/RiskFree.xlsx');
+            obj.risk_free_rate = table2timetable(obj.risk_free_rate,'RowTimes',obj.dates);
+            obj.risk_free_rate.Date = []
             
+            obj.dates = sort([datetime([1976:2012]', 1, 1);
+                datetime([1976:2012]', 4, 1);
+                datetime([1976:2012]', 7, 1);
+                datetime([1976:2012]', 10, 1)]);
             
             obj.mu_credit_exp = fillmissing(obj.mu_credit_exp, 'nearest');
             obj.historical_returns = fillmissing(obj.historical_returns, 'nearest');
-        
+            
+            
+            
+            
         end
         
+        %follows example on the page 5 of The Black - Litterman Approach by
+        %Attilio Meucci
         function [P, v]  = portfolioViews(obj,date)
-            P = eye(20);
+            P = eye(obj.portfolio_size);
             v = table2array(obj.mu_credit_exp(date,1:end))';
         end
         
@@ -54,66 +72,100 @@ classdef BlackLitterman
             sigma = cov(table2array(obj.historical_returns(tr,1:end)));
         end
         
+        % all equations are from Meucci
         function [mu_bl, sigma_bl] = blackLittermanModel(obj, P,v, mu,sigma)
             [m,n] = size(P);
-            w = ones(n,1)/n;
+            w = ones(n,1)/(n+1);
             tau = 1/m;
             pi = 2.4*sigma*w;
             
             %c=1 - no uncertaity in the views
             Omega = P*sigma*P';
-
+            
+            %eq 20
             mu_bl = pi + tau*sigma*P'*inv(tau*P*sigma*P' + Omega)*(v-P*pi);
+            
+            %eq21
             sigma_bl = (1+tau)*sigma - tau^2 * sigma * P'*inv(tau*P*sigma*P' + Omega)*P*sigma;
             
         end
         
-        function weights = markovitz(obj, mu, V)
-                   
-            n = length(mu);
-            e = ones(n,1);
+        function weights = markowitz (obj, mu0, mu, V, benchmark_risk, Sample_Returns)
             U = chol(V);
+            S_R = table2array(Sample_Returns);
             
-            sigma = 0.2;
+            [T,n] = size(S_R);
             
             cvx_begin quiet
             
-            variables x(n)
-            
-            maximize(mu'*x);
+            variables x0 x(n)
+            maximize(x0*mu0 + mu'*x)
             
             subject to
-                norm(U*x) <= sigma;
-                sum(x) == 1;
-                abs(x)<=0.5;
+            norm(U*x) <= benchmark_risk;
+            sum(x) + x0== 1
+            x0>=0
+            abs(x - 0.2)<=0.2;
             cvx_end
+            weights = [x0; x];
             
-            weights = [x]
         end
         
         function port_returns = simulate(obj)
             port_returns = [];
+            w= [];
             old_date = obj.dates(40);
             for date = obj.dates(41:end)'
+                mu0 = (1+obj.risk_free_rate{old_date, 'US'})^(0.25) - 1;
+                mu0_future = (1+obj.risk_free_rate{date, 'US'})^(0.25) - 1;
+                e = ones(obj.portfolio_size,1);	
+
                 [P, v ] = obj.portfolioViews(old_date);
                 [mu, sigma] = obj.portfolioMuCov(old_date);
+                benchmark_risk = sqrt(e'*sigma*e)/(obj.portfolio_size);
                 [mu_bl, sigma_bl] = obj.blackLittermanModel(P,v, mu,sigma);
-                weights = obj.markovitz(mu_bl, sigma_bl);
-                port_returns = [port_returns;table2array(obj.historical_returns(date,:))*weights]
+                weights = obj.markowitz (mu0, mu_bl, sigma_bl, benchmark_risk, obj.historical_returns(timerange(obj.dates(1),old_date),:));
+                port_returns = [port_returns;mu0_future*weights(1)+obj.historical_returns{date,:}*weights(2:end)];
+                w = [w;weights'];
                 old_date = date;
             end
-
+            xlswrite('weights_bl.xlsx',w);
         end
         
-        function port_returns = simulate_markovitz(obj)
+        function port_returns = simulate_markowitz (obj)
             port_returns = [];
+            w= [];
             old_date = obj.dates(40);
             for date = obj.dates(41:end)'
+                mu0 = (1+obj.risk_free_rate{old_date, 'US'})^(0.25) - 1;
+                mu0_future = (1+obj.risk_free_rate{date, 'US'})^(0.25) - 1;
+                e = ones(obj.portfolio_size,1);	
+                [P, v ] = obj.portfolioViews(old_date);
                 [mu, sigma] = obj.portfolioMuCov(old_date);
-                weights = obj.markovitz(mu, sigma);
-                port_returns = [port_returns;table2array(obj.historical_returns(date,:))*weights]
+                benchmark_risk = sqrt(e'*sigma*e)/(obj.portfolio_size);
+                weights = obj.markowitz (mu0, mu, sigma,benchmark_risk, obj.historical_returns(timerange(obj.dates(1),old_date),:));
+                port_returns = [port_returns;mu0_future*weights(1)+obj.historical_returns{date,:}*weights(2:end)];
                 old_date = date;
+                w = [w;weights'];
             end
+            xlswrite('weights.xlsx',w);
+        end
+        
+        function w = run(obj, ttl, fig_ind)
+            returns_bl = obj.simulate();
+            returns_m = obj.simulate_markowitz();
+
+            portf_bl = cumprod(1+returns_bl);
+            portf = cumprod(1+returns_m);
+
+            figure
+                plot(obj.dates(42:end), portf,obj.dates(42:end), portf_bl)
+
+                title(ttl)
+                legend('Markowitz', 'Black-Litterman', 'Location', 'NorthWest'); 
+                savefig(strcat('plots/fig',int2str(fig_ind), '.fig'));
+                
+            w = [ttl, portf_bl(end), portf(end)];
         end
     end
 end
--- CreditExpansion.m
+++ CreditExpansion.m
@@ -1,6 +1,55 @@
-bl = BlackLitterman
-returns1 = bl.simulate();
+all_countries = {'Australia' 'Austria' 'Belgium' 'Canada' 'Denmark' 'France' 'Germany' 'Ireland' 'Italy' 'Japan' 'Korea' 'Netherlands' 'Norway' 'Singapore' 'Spain' 'Sweden' 'Switzerland' 'UK' 'US'};
+bank_countries = {'Australia' 'Belgium' 'Canada' 'Denmark' 'France' 'Germany' 'Ireland' 'Italy' 'Japan' 'Netherlands' 'Norway' 'Spain' 'Sweden' 'Switzerland' 'UK' 'US'};
+i = 13;
+ret = [];
 
-returns_m = bl.simulate_markovitz();
 
-plot(bl.dates(41:end), cumprod(1+returns_m),bl.dates(41:end), cumprod(1+returns1))
+
+bl = BlackLitterman('data/predictions/bank_return_1y.xlsx', bank_countries,'data/historical data/unhedged_histdata.xlsx', 'bank returns', 4);
+ret = [ret;bl.run('1 year prediction. Unhedged bank returns', i)];
+i=i+1;
+
+bl = BlackLitterman('data/predictions/bank_return_2y.xlsx',bank_countries, 'data/historical data/unhedged_histdata.xlsx', 'bank returns', 8);
+ret = [ret;bl.run('2 year prediction. Unhedged bank returns', i)];
+i=i+1;
+
+bl = BlackLitterman('data/predictions/bank_return_3y.xlsx', bank_countries, 'data/historical data/unhedged_histdata.xlsx', 'bank returns', 12);
+ret = [ret;bl.run('3 year prediction. Unhedged bank returns',i)];
+i=i+1;
+
+bl = BlackLitterman('data/predictions/non_financial_return_1y.xlsx',all_countries, 'data/historical data/unhedged_histdata.xlsx', 'total returns', 4);
+ret = [ret;bl.run('1 year prediction. Unhedged total returns',i)];
+i=i+1;
+
+bl = BlackLitterman('data/predictions/non_financial_return_2y.xlsx',all_countries, 'data/historical data/unhedged_histdata.xlsx', 'total returns', 8);
+ret = [ret;bl.run('2 year prediction. Unhedged total returns',i)];
+i=i+1;
+
+bl = BlackLitterman('data/predictions/non_financial_return_3y.xlsx',all_countries, 'data/historical data/unhedged_histdata.xlsx', 'total returns', 12);
+ret = [ret;bl.run('3 year prediction. Unhedged total returns',i)];
+i=i+1;
+
+bl = BlackLitterman('data/predictions/bank_return_1y.xlsx',bank_countries, 'data/historical data/hedged_histdata.xlsx', 'bank returns', 4);
+ret = [ret;bl.run('1 year prediction. Hedged bank returns',i)];
+i=i+1;
+
+bl = BlackLitterman('data/predictions/bank_return_2y.xlsx',bank_countries, 'data/historical data/hedged_histdata.xlsx', 'bank returns', 8);
+ret = [ret;bl.run('2 year prediction. Hedged bank returns',i)];
+i=i+1;
+
+bl = BlackLitterman('data/predictions/bank_return_3y.xlsx',bank_countries, 'data/historical data/hedged_histdata.xlsx', 'bank returns', 12);
+ret = [ret;bl.run('3 year prediction. Hedged bank returns',i)];
+i=i+1;
+
+bl = BlackLitterman('data/predictions/non_financial_return_1y.xlsx',all_countries, 'data/historical data/hedged_histdata.xlsx', 'total returns', 4);
+ret = [ret;bl.run('1 year prediction. Hedged total returns',i)];
+i=i+1;
+
+bl = BlackLitterman('data/predictions/non_financial_return_2y.xlsx',all_countries, 'data/historical data/hedged_histdata.xlsx', 'total returns', 8);
+ret = [ret;bl.run('2 year prediction. Hedged total returns',i)];
+i=i+1;
+
+bl = BlackLitterman('data/predictions/non_financial_return_3y.xlsx',all_countries, 'data/historical data/hedged_histdata.xlsx', 'total returns', 12);
+ret = [ret;bl.run('3 year prediction. Hedged total returns',i)];
+i=i+1;
+xlswrite('returns.xlsx',ret);
